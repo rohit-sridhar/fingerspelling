5366ccaed1cab6356770ecfffb27d5ff0e23ea2e
Sridhar <rsridhar37@ebisu.cc.gatech.edu>
2024-03-17 16:41:49 -0400

    Modified run docker script logic

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,69 +1,74 @@
 #!/bin/bash
 
-# root="/home/lab-workstation/ServerData"
-root="$1"
+if [ "$1" == "ls" ]; then
+    sudo docker ps -a
+    exit 0
+else
+    if [ "$2" == "" ]; then
+        echo "Specify a container name when calling launch, run or rm"
+        exit 1
+    fi
+    
+    if [ "$1" == "run" ]; then
+        sudo docker exec -it $2 /bin/bash
+    elif [ "$1" == "rm" ]; then
+        sudo docker stop $2
+        sudo docker rm $2
+    elif [ "$1" == "launch" ]; then
+        if [ "$3" == "" ]; then
+            echo "Specify a local root for local path mounts when calling launch."
+            exit 1
+        fi
+        
+        root="$3"
+        
+        mb_pipe_path="$root/Mobile-Data-Processing-Pipeline"
+        islr_path="$root/hmm_modeling/islr/ContinuousBigram"
+        phrase_data_path="$root/sign_language_videos/phrase_data"
+        
+        if [ "$2" == "islr" ]; then
+            sudo docker run \
+                -dit \
+                -v "$mb_pipe_path":/root/Mobile-Data-Processing-Pipeline \
+                -v "$islr_path":/root/islr/ContinuousBigram \
+                --name $2 rohitsridhar91/asl_sign_recognizer:v1.2
+        elif [ "$2" == "phrase" ]; then
+            sudo docker run \
+                -dit \
+                -v "$mb_pipe_path":/root/Mobile-Data-Processing-Pipeline \
+                -v "$phrase_data_path":/root/phrase_data \
+                --name $2 rohitsridhar91/asl_sign_recognizer:v1.2
+        elif [ "$2" == "fingerspelling" ]; then
+            continuous_bigram_path="$root/hmm_modeling/fingerspelling/ContinuousBigram"
+            continuous_bigram_ss_path="$root/hmm_modeling/silentspeller/ContinuousBigram"
+            
+            sudo docker run \
+                -dit \
+                -v "$continuous_bigram_ss_path":/root/silentspeller/ContinuousBigram \
+                -v "$continuous_bigram_path":/root/fingerspelling/ContinuousBigram \
+                --name $2 rohitsridhar91/asl_sign_recognizer:v1.2
+        elif [ "$2" == "fingerspelling_matthew" ]; then
+            continuous_bigram_path_matthew="$root/hmm_modeling/matthew/ContinuousBigram"
+            
+            sudo docker run \
+                -dit \
+                -v "$continuous_bigram_path_matthew":/root/fingerspelling/ContinuousBigram \
+                --name $2 rohitsridhar91/asl_sign_recognizer:v1.2
+        fi
+        
+        if [ "$4" != "" ]; then
+            sudo docker cp "$4" $2:/root
+            echo "$4 copied to $2:/root"
+        fi
+    else
+        echo "Specify either launch (create the container), run (resume the existing container), rm (remove existing container), or ls"
+    fi
+fi
 
-mb_pipe_path="$root/Mobile-Data-Processing-Pipeline"
-phrase_data_path="$root/sign_language_videos/phrase_data"
-parquet_path="$root/parquet"
+# root="/home/lab-workstation/ServerData"
+
+# parquet_path="$root/parquet"
 
 # continuous_bigram_dim8_path="$root/hmm_modeling/fingerspelling/ContinuousBigram.dim8"
 # continuous_bigram_dim12_path="$root/hmm_modeling/fingerspelling/ContinuousBigram.dim12"
-continuous_bigram_path="$root/hmm_modeling/fingerspelling/ContinuousBigram"
-islr_path="$root/hmm_modeling/islr/ContinuousBigram"
 
-continuous_bigram_ss_path="$root/hmm_modeling/silentspeller/ContinuousBigram"
-continuous_bigram_path_matthew="$root/hmm_modeling/matthew/ContinuousBigram"
-
-if [ "$2" == "ls" ]; then
-    sudo docker ps -a
-    exit 0
-fi
-
-if [ "$3" == "" ]; then
-    echo "Specify a container name when calling launch, run or rm"
-    exit 0
-fi
-
-if [ "$2" == "launch" ]; then
-    if [ "$3" == "islr" ]; then
-        sudo docker run \
-            -dit \
-            -v "$mb_pipe_path":/root/Mobile-Data-Processing-Pipeline \
-            -v "$islr_path":/root/islr/ContinuousBigram \
-            --name $3 rohitsridhar91/asl_sign_recognizer:v1.2
-    elif [ "$3" == "phrase" ]; then
-        sudo docker run \
-            -dit \
-            -v "$mb_pipe_path":/root/Mobile-Data-Processing-Pipeline \
-            -v "$phrase_data_path":/root/phrase_data \
-            --name $3 rohitsridhar91/asl_sign_recognizer:v1.2
-    elif [ "$3" == "fingerspelling" ]; then
-        sudo docker run \
-            -dit \
-            -v "$mb_pipe_path":/root/Mobile-Data-Processing-Pipeline \
-            -v "$parquet_path":/root/kaggle \
-            -v "$continuous_bigram_ss_path":/root/silentspeller/ContinuousBigram \
-            -v "$continuous_bigram_path":/root/fingerspelling/ContinuousBigram \
-            --name $3 rohitsridhar91/asl_sign_recognizer:v1.2
-    elif [ "$3" == "fingerspelling_matthew" ]; then
-        sudo docker run \
-            -dit \
-            -v "$mb_pipe_path":/root/Mobile-Data-Processing-Pipeline \
-            -v "$parquet_path":/root/kaggle \
-            -v "$continuous_bigram_path_matthew":/root/fingerspelling/ContinuousBigram \
-            --name $3 rohitsridhar91/asl_sign_recognizer:v1.2
-    fi
-    
-    if [ "$4" != "" ]; then
-        sudo docker cp "$4" $3:/root
-        echo "$4 copied to $3:/root"
-    fi
-elif [ "$2" == "run" ]; then
-    sudo docker exec -it $3 /bin/bash
-elif [ "$2" == "rm" ]; then
-    sudo docker stop $3
-    sudo docker rm $3
-else
-    echo "Specify either launch (create the container), run (resume the existing container), rm (remove existing container), or ls"
-fi
