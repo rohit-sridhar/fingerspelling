2995085ecdd218f59a17ed9022b7870b831820ab
Rohit Sridhar <rohit.sridhar91@gmail.com>
2024-04-25 13:46:17 -0400

    modifications to hierarchy

--- ContinuousBigram/scripts/options.sh
+++ ContinuousBigram/scripts/options.sh
@@ -34,7 +34,7 @@ MIN_VARIANCE=0.01				# don't let the
 						# this value during
 						# HMM training
 
-INSERT_PENALTY=-10	#Penalize model for too many word insertion/deletion
+INSERT_PENALTY=20.0	#Penalize model for too many word insertion/deletion
 						#If too many deletions, increase
 						#If too many insertions, decrease
 
@@ -44,8 +44,8 @@ SAMPLE_PERIOD=1000
 
 MULTI_PROCESS="yes"
 # THREADS=8           # For Hotei
-THREADS=32        # For Ebisu
-# THREADS=96        # For Benten
+# THREADS=32        # For Ebisu
+THREADS=96        # For Benten
 
 #PRUNING_THRESHOLD="50 50 500" #Threshold for alpha-beta pruning, of form "start step-size end"
 PRUNING_THRESHOLD=0
@@ -53,7 +53,7 @@ PRUNING_THRESHOLD=0
 HMM_TOPOLOGY_DIR=${PRJ}/hmmdefs
 
 # general HMM_TOPOLOGIES
-HMM_LOCATION=$HMM_TOPOLOGY_DIR/6state-pca20-gmm4
+HMM_LOCATION=$HMM_TOPOLOGY_DIR/6state-pca20-gmm4-skip126
 HMM_ALL=$HMM_LOCATION
 HMM_SIL=$HMM_TOPOLOGY_DIR/3state-pca20-sil-skip-loop
 HMM_SP=$HMM_TOPOLOGY_DIR/1state-pca20-sp
@@ -62,7 +62,7 @@ ENTER="sil0"
 EXIT="sil1"
 SP="_"
 
-BIGRAM_LETTER=yes   # Whether a bigram word net should be used (letter level)
+BIGRAM_LETTER=no   # Whether a bigram word net should be used (letter level)
 BIGRAM_WORD=no   # Whether a bigram word net should be used (letter level)
 CUSTOM_SILSP=yes   # Whether HMM_SIL/HMM_SP should be used
 
@@ -184,8 +184,8 @@ OUTPUT_MLF=${EXT_DIR}/result.mlf_letter		# where HTK stores results
 						# .ext files
 OUTPUT_MLF_WORD=${EXT_DIR}/result.mlf_word
 
-LOG_RESULTS=${PRJ}/results/dim20/thr8/grliwi/sten/hresults.log_letter_neg10ip_6state-pca20-gmm4_20its_5tri-its_silsp_bgl
-LOG_RESULTS_WORD=${PRJ}/results/dim20/thr8/grliwi/sten/hresults.log_word_neg10ip_6state-pca20-gmm4_20its_5tri-its_silsp_bgl
+LOG_RESULTS=${PRJ}/results/dim20/thr8/grliwi/hresults.log_letter_pos20ip_6state-pca20-gmm4-skip126_20its_5tri-its_silsp
+LOG_RESULTS_WORD=${PRJ}/results/dim20/thr8/grliwi/hresults.log_word_pos20ip_6state-pca20-gmm4-skip126_20its_5tri-its_silsp
 
 HMM_TEMP_DIR=${PRJ}/models			# directory for storing
 						# intermediate models during
