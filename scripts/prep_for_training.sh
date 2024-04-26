cac25cc5c2ae8e5bd737e0996789e2736b0357c2
Rohit Sridhar <rohit.sridhar91@gmail.com>
2024-04-26 19:42:56 -0400

    pushing prep_for_training

---
+++ scripts/prep_for_training.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ln -s $1/logs $2/logs
+ln -s $1/data $2/data
+ln -s $1/label $2/label
+ln -s $1/results $2/results
+ln -s $1/commands $2/commands
+ln -s $1/dict $2/dict
+ln -s $1/grammar $2/grammar
+ln -s $1/mlf $2/mlf
+
