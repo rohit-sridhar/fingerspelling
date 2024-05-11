5aa942752ec82cc328386431cb48eb02f3205418
Sridhar <rsridhar37@ebisu.cc.gatech.edu>
2024-05-10 20:48:09 -0400

    actually removing the sym links this time

--- scripts/prep_for_training.sh
+++ scripts/prep_for_training.sh
@@ -4,10 +4,6 @@ ln -s $1/logs $2/logs
 ln -s $1/data $2/data
 ln -s $1/label $2/label
 ln -s $1/results $2/results
-ln -s $1/commands $2/commands
-ln -s $1/dict $2/dict
-ln -s $1/grammar $2/grammar
-ln -s $1/mlf $2/mlf
 
 mkdir $2/ext
 mkdir $2/models
@@ -16,6 +12,7 @@ mkdir $2/trainsets
 mkdir $2/testsets
 touch $2/trainsets/training-extfiles0
 touch $2/testsets/testing-extfiles0
+
 cp -r $1/commands $2/
 cp -r $1/dict $2/
 cp -r $1/grammar $2/
