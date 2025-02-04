aaab9f60e4abf45f6de683ac9270271f8086f358
Rohit Sridhar <rohit.sridhar91@gmail.com>
2025-02-03 21:34:51 -0500

    Make the models dir first

--- scripts/prep_for_training.sh
+++ scripts/prep_for_training.sh
@@ -5,18 +5,20 @@
 ## Arg 2: Absolute path to new fingerspelling directory (hard link metadata to)
 ##### IMPORTANT: The paths must end with ContinuousBigram/
 
-ln -s $1/logs $2/logs
-ln -s $1/data $2/data
-ln -s $1/label $2/label
-ln -s $1/results $2/results
-ln -s $1/models/supplemental $2/models/supplemental
-
 mkdir $2/ext
+mkdir $2/models
 mkdir $2/output
 mkdir $2/trainsets
 mkdir $2/testsets
 touch $2/trainsets/training-extfiles0
 touch $2/testsets/testing-extfiles0
+
+
+ln -s $1/logs $2/logs
+ln -s $1/data $2/data
+ln -s $1/label $2/label
+ln -s $1/results $2/results
+ln -s $1/models/supplemental $2/models/supplemental # This will be generalized later
 
 cp -r $1/commands $2/
 cp -r $1/dict $2/
