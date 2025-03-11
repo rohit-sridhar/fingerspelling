762d8d546315854d0931473ac4b169496dd31867
Rohit Sridhar <rsridhar37@login-phoenix-rh9-3.pace.gatech.edu>
2025-03-11 01:28:53 -0400

    Pushing slashdata pace transfer changes

--- scripts/slashdata.sh
+++ scripts/slashdata.sh
@@ -4,12 +4,16 @@
 ##### Copy HTK Files over
 
 # ContinuousBigram/commands/supplemental/dl_cmp/dim20/thr1/train/grpfpl311/sd1248/commands_tri_internal
-rsync -rH --progress ./ContinuousBigram/commands/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/commands/
-rsync -rH --progress ./ContinuousBigram/dict/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/dict/
-rsync -rH --progress ./ContinuousBigram/grammar/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/grammar/
-rsync -rH --progress ./ContinuousBigram/logs/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/logs/
-rsync -rH --progress ./ContinuousBigram/mlf/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/mlf/
-rsync -rH --progress ./ContinuousBigram/models/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/models/
-rsync -rH --progress ./ContinuousBigram/output/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/output/
-rsync -rH --progress ./ContinuousBigram/results/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/results/
+scp -r ./ContinuousBigram/commands/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/commands/
+scp -r ./ContinuousBigram/dict/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/dict/
+scp -r ./ContinuousBigram/grammar/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/grammar/
+scp -r ./ContinuousBigram/logs/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/logs/
+scp -r ./ContinuousBigram/mlf/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/mlf/
+
+##### Redo with rsync pattern matching to exclude acc files
+# scp -r ./ContinuousBigram/models/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/models/
+##### 
+
+scp -r ./ContinuousBigram/output/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/output/
+scp -r ./ContinuousBigram/results/supplemental rsridhar37@benten.cc.gatech.edu:/data/hmm_modeling/fingerspelling/ContinuousBigram/results/
 
