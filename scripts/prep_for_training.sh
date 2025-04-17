a29993c651f853b263472f9e3386ce3f6a11a719
Rohit Sridhar <rsridhar37@login-phoenix-rh9-1.pace.gatech.edu>
2025-04-17 11:05:43 -0400

    Adding val script and minor changes

--- scripts/prep_for_training.sh
+++ scripts/prep_for_training.sh
@@ -5,34 +5,33 @@
 ## Arg 2: Absolute path to new fingerspelling directory (hard link metadata to)
 ##### IMPORTANT: The paths must end with ContinuousBigram/
 
-mkdir $2/ext
-mkdir $2/models
-mkdir $2/output
-
-cp -rl $1/logs $2/logs
-cp -rl $1/data $2/data
-cp -rl $1/label $2/label
-cp -rl $1/results $2/results
-cp -rl $1/commands $2/commands
-cp -rl $1/dict $2/dict
-cp -rl $1/grammar $2/grammar
-cp -rl $1/mlf $2/mlf
-cp -rl $1/models/supplemental $2/models/supplemental # This will be generalized later
+# mkdir $2/ext
+# mkdir $2/models
+# mkdir $2/output
+# 
+# cp -rl $1/logs $2/logs
+# cp -rl $1/data $2/data
+# cp -rl $1/label $2/label
+# cp -rl $1/results $2/results
+# cp -rl $1/commands $2/commands
+# cp -rl $1/dict $2/dict
+# cp -rl $1/grammar $2/grammar
+# cp -rl $1/mlf $2/mlf
+# cp -rl $1/models/supplemental $2/models/supplemental # This will be generalized later
 
 
 # Use these commands when transferring to PACE (Commented out for now).
-# mkdir ContinuousBigram/ext
-# mkdir ContinuousBigram/models
-# mkdir ContinuousBigram/output
-# mkdir ContinuousBigram/logs
-# mkdir -p ContinuousBigram/data/supplemental/dl_cmp/dim20/thr0/train
-# mkdir -p ContinuousBigram/data/supplemental/dl_cmp/dim20/thr0/val
-# mkdir -p ContinuousBigram/label/supplemental/dl_cmp/thr0/train
-# mkdir -p ContinuousBigram/label/supplemental/dl_cmp/thr0/val
-# mkdir ContinuousBigram/results
-# mkdir ContinuousBigram/commands
-# mkdir ContinuousBigram/dict
-# mkdir ContinuousBigram/grammar
-# mkdir ContinuousBigram/mlf
-
+mkdir ContinuousBigram/ext
+mkdir ContinuousBigram/models
+mkdir ContinuousBigram/output
+mkdir ContinuousBigram/logs
+mkdir -p ContinuousBigram/data/supplemental/dl_cmp/dim20/thr0/train
+mkdir -p ContinuousBigram/data/supplemental/dl_cmp/dim20/thr0/val
+mkdir -p ContinuousBigram/label/supplemental/dl_cmp/thr0/train
+mkdir -p ContinuousBigram/label/supplemental/dl_cmp/thr0/val
+mkdir ContinuousBigram/results
+mkdir ContinuousBigram/commands
+mkdir ContinuousBigram/dict
+mkdir ContinuousBigram/grammar
+mkdir ContinuousBigram/mlf
 
