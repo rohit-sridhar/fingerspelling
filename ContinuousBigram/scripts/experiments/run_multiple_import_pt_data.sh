59017c75b0c7d9d09a6bf059758ca476311a6b24
Rohit Sridhar <rsridhar37@login-phoenix-gnr-1.pace.gatech.edu>
2026-06-26 16:38:51 -0400

    added slurm functionality to import pt data

--- ContinuousBigram/scripts/experiments/run_multiple_import_pt_data.sh
+++ ContinuousBigram/scripts/experiments/run_multiple_import_pt_data.sh
@@ -11,23 +11,33 @@ echo ""
 echo "STARTING IMPORT"
 echo ""
 
-participants=(ab12)
+# participants=(3f8b 13e3 494d b2d1 ab12)
+if [[ -v SLURM_ARRAY_TASK_ID ]]; then
+    if [[ ${SLURM_ARRAY_TASK_ID} -ge ${#participants[@]} ]]; then
+        echo "The SLUM ARRAY TASK ID is too large. Should be <= num participants"
+        exit 1
+    fi
+    participants=(${participants[${SLURM_ARRAY_TASK_ID}]})
+else
+    participants=${participants[@]}
+fi
+
 ############################## IMPORT MULTIPLE (TRAIN,VAL,TEST) ##############################
 
 for dataset in ${datasets[@]}; do
 for data_split in ${data_splits[@]}; do
 for seed in "${seeds[@]}"; do
 bp=0
-pid=()
+# pid=()
 for participant in "${participants[@]}"; do
     ${ROOT}/scripts/modify_data.py \
         --import_data_loc ${TORCH_ROOT}/data/data_${dataset}_sd${seed}_pt-${participant}_rh.pq.${data_split} \
         --new_data_loc ${ROOT}/data/${dataset}/dim20/thr0/${data_split}/pt/${participant}/sd${seed}/data \
-        --method import --bar_position ${bp} --bar_description "${data_split}|${seed}|${participant}" &
+        --method import --bar_position ${bp} --bar_description "${data_split}|${seed}|${participant}"
     bp=$((bp+1))
-    pid+=("$!")
+#     pid+=("$!")
 done
-wait "${pid[@]}"
+# wait "${pid[@]}"
 done
 done
 done
@@ -41,7 +51,7 @@ done
 #     ${ROOT}/scripts/modify_data.py \
 #         --import_data_loc ${TORCH_ROOT}/data/data_${dataset}_sd${seed}_pt-${participant}_pca10_rh.pq.${data_split} \
 #         --new_data_loc ${ROOT}/data/${dataset}/pca10/thr0/${data_split}/pt/${participant}/sd${seed}/data \
-#         --method import --bar_position ${bp} --bar_description "${data_split}|${seed}|${participant}" &
+#         --method import --bar_position ${bp} --bar_description "${data_split}|${seed}|${participant}"
 #     bp=$((bp+1))
 #     pid+=("$!")
 # done
@@ -49,4 +59,4 @@ done
 # done
 # done
 # done
-# 
+
